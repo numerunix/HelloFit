93897a902eb7b55422392a755e03e9a5cd96913e
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-23 12:17:10 +0200

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -4,7 +4,7 @@
       <Section>
         {<Text bold align="center">Creato da Giulio Sorrentino</Text>}
         {<Text bold align="center">Dedicato a Francesca Milano</Text>}}
-        {<Text bold align="center">Questo programma è sotto licenza GPL v3 o, secondo il tuo parere, qualsiasi versione successiva</text>}
+        {<Text bold align="center">Questo programma è sotto licenza GPL v3 o, secondo il tuo parere, qualsiasi versione successiva</Text>}
         {<Text bold align="center">https://www.github.com/numerunix/HelloFit</Text>}
       </Section>
     </Page>
