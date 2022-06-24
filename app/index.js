3760cd13be4a898eaadbbb8be651bc48e2adeafb
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-24 02:20:30 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,4 +4,8 @@
 
 import { gettext } from "i18n";
 
+console.log("Copyright \u00A92022 Giulio Sorrentino<gsorre84@gmail.com>");
+console.log("This progam is dedicated to Francesca Milano");
+console.log("This program is inspired by commercial product \"Days Together\"");
+console.log("This program is distribuited under GPL. No Warranty is provided.");
 console.log("App code started");
